713bd1e94394b1aa4466b16f86874f98855e9fed
Zartaj2007 <zartajalichanna25@gmail.com>
2025-01-12 18:19:15 +0500

    Update vitaClassicProduct.tsx

--- src/app/components/vitaClassicProduct.tsx
+++ src/app/components/vitaClassicProduct.tsx
@@ -25,7 +25,7 @@ const VitaClassicProduct = () => {
           <Image
             src="/shop-hero.png"
             alt="Vita Classic Product"
-            width={550}
+            width={500}
             height={500}
             className="w-full h-full"
           />
