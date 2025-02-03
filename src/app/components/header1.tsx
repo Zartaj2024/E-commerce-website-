99fd6a215f41237db781dfaea7086788d2b878f9
Zartaj2007 <zartajalichanna25@gmail.com>
2025-02-03 19:36:16 +0500

    update header

--- src/app/components/header1.tsx
+++ src/app/components/header1.tsx
@@ -1,61 +1,39 @@
 import React from "react";
 import Image from "next/image";
-
-const Header1 = () => {
+export default function Header() {
   return (
-    <div className="hidden md:flex justify-between items-center pb-3 pt-3 font-bold p-3 w-[1439px] h-[58px]">
-      <div className="w-[1438px] h-[46px] ">
-        <div className="flex gap-10 w-[415px] h-[46px] ">
-          <div className="flex pt-3 pb-3 gap-2 w-[145px] h-[45px] pb-5 pt-5">
-            <Image
-              src="/phone.png"
-              alt="phone"
-              className="w-[16px] h-[16px]"
-              width={16}
-              height={16}
-            />
-            <div>
-              <h6 className="w-[104px] h-[24px]">(225) 555-0118</h6>
+    <header className="bg-[#FAFAFA] w-[1440px] h-[852px]">
+      <div className="w-[1439px] h-[136px] bg-[#252B42]">
+        <div className="w-[1439px] h-[58px]">
+          <div className="w-[1438px] h-[46px] pt-9 pl-2">
+            <div className="flex w-[1438px] h-[46px] justify-space-between">
+              <div className="w-[1438px] h-[46px] justify-space-between pl-[24px] pr-[24px]">
+                <div className="w-[415px] h-[46px] gap-10">
+                  <div className="grid grid-cols-2 w-[145px] h-[44px] rounded-[5px] p-10 gap-[5px]">
+                    <div className="w-[16px] h-[16px]">
+                      <Image
+                        src="/phone.png"
+                        alt="Image description"
+                        width={16}
+                        height={16}
+                        className="w-[16px] h-[16px]"
+                      />
+                    </div>
+                    <h6 className="w-[104px] h-[24px] font-montserrat text-[14px] font-bold leading-[24px] tracking-[0.2px]">
+                      {"(225) 555-0118"}
+                    </h6>
+                  </div>
+                  <div className="w-[332px] h-[44px] p-[10px] gap-[10px] ">
+                    <h6 className="font-montserrat text-[14px] font-bold leading-[24px] tracking-[0.2px] ">
+                      Follow Us and get a chance to win 80% off
+                    </h6>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="flex gap-1 w-[260px] h-[44px] pb-5 pt-5">
-            <Image
-              src="/mail.png"
-              alt="mail"
-              className="w-[16px] h-[16px]"
-              width={16}
-              height={16}
-            />
-
-            <div>
-              <h6 className="w-[219px] h-[24px]">
-                michelle.rivera@example.com
-              </h6>
-            </div>
-          </div>
-        </div>
-
-        <div className="flex pt-3 pb-3 w-[332px] h-[44px]">
-          <p className="w-[312px] h-[24px]">
-            Follow Us and get a chance to win 80% off
-          </p>
-        </div>
-
-        <div className="flex py-5 px-5 w-[233px] h-[46px] ">
-          <p className="flex">
-            <Image
-              src="/social media.png"
-              alt="media"
-              className="w-[124px] h-[26px]"
-              width={124}
-              height={26}
-            />
-          </p>
-          <h6 className="w-[83px]  h-[24px]">Follow Us :</h6>
         </div>
       </div>
-    </div>
+    </header>
   );
-};
-
-export default Header1;
+}
